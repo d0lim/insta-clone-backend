ef9bbf535c12c8dd8d1b0158167c070444b7a438
imdigo <limdongyoung0@gmail.com>
2021-07-23 21:33:55 +0900

    fix: update swagger docs from username to email

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -5,9 +5,9 @@ export class LogInDto {
   @IsString()
   @ApiProperty({
     type: String,
-    description: `User's email. Cause of passport, we need to user username not email. But input proper email here.`,
+    description: `User's email.`,
   })
-  readonly username: string;
+  readonly email: string;
 
   @IsString()
   @ApiProperty({
